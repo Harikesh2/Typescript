b0de95e0e942efb70744d02a057bd1bad29c809e
Harikesh2 <mths83478@gmail.com>
2024-01-19 21:31:46 +0530

    working on readonly one

---
+++ 02Basic/ReadonlyAndOptional.ts
@@ -0,0 +1,25 @@
+
+
+type User = {
+    readonly _id: string,
+    name: string,
+    email: string,
+    isActive: boolean,
+    credcardDetails?: number
+    // optional syntax is this user can give it or not
+}
+// readonly variable is only readable we cannot change it!
+
+let myUser : User = {
+    _id: "12345",
+    name: "h",
+    email: "h@h.com",
+    isActive: false
+}
+
+myUser.name = "Harikesh"
+//  possible in this
+
+// myUser._id = 'asd'
+
+// will give error!
